425e3a991670602b89976f49b9401d8eade997ec
Peter Smith <comprs@outlook.com>
2015-03-12 13:56:54 +0000

    Add error managing by displaying a message box

--- main.cpp
+++ main.cpp
@@ -11,7 +11,16 @@ int main()
 
     Defender::Game game;
     game.replaceNewRoom<Defender::MainMenu>();
-    game.begin();
+
+    // Start the game. Catch any error that may occur and display a messagebox
+    // stating the error.
+    try { game.begin(); }
+    catch (std::runtime_error e)
+    {
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR,
+                                 "A runtime error occured",
+                                 e.what(), nullptr);
+    }
 
     SDL_Quit();
 
