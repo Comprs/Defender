7d131aaf728362ffd09f9d1859354c72c3bb11de
Peter Smith <comprs@outlook.com>
2015-03-17 09:43:20 +0000

    Made the N increment value a parameter. Called it D

--- pseudorandomdistribution.h
+++ pseudorandomdistribution.h
@@ -14,20 +14,24 @@ public:
     {
     public:
         typedef pseudo_random_distribution distribution_type;
-        explicit param_type(double c) : parameter_c(c) {}
+        explicit param_type(double c, double d) : parameter_c(c),
+            parameter_d(d) {}
         double c() const { return parameter_c; }
+        double d() const { return parameter_d; }
 
         friend bool operator == (const param_type& lhs,
                                  const param_type& rhs)
         {
-            return lhs.parameter_c == rhs.parameter_c;
+            return lhs.parameter_c == rhs.parameter_c &&
+                    lhs.parameter_d == rhs.parameter_d;
         }
 
     private:
         double parameter_c;
+        double parameter_d;
     };
-    explicit pseudo_random_distribution(double c = 0.5) : parameters(c),
-        internal_n(0) {}
+    explicit pseudo_random_distribution(double c = 0.5, double d = 0.009) :
+        parameters(c, d), internal_n(0) {}
     explicit pseudo_random_distribution(const param_type& params) :
         parameters(params) {}
 
@@ -35,6 +39,7 @@ public:
     void param(const param_type& params) { parameters = params; }
 
     double c() const { return parameters.c(); }
+    double d() const { return parameters.d(); }
 
     result_type min() { return std::numeric_limits<result_type>::min(); }
     result_type max() { return std::numeric_limits<result_type>::max(); }
@@ -47,7 +52,7 @@ public:
         double thisChance = gen_params.c() * internal_n;
         bool success = (g() - g.min()) < thisChance * (g.max() - g.min());
         if (success) { internal_n = 1; }
-        else { internal_n += 0.0085; }
+        else { internal_n += gen_params.d(); }
         return success;
     }
 
@@ -80,7 +85,7 @@ std::basic_ostream<CharT, Traits>&
 operator << (std::basic_ostream<CharT, Traits>& os,
              const pseudo_random_distribution& d)
 {
-    os << d.parameters.parameter_c << d.internal_n;
+    os << d.parameters.parameter_c << d.parameters.parameter_d << d.internal_n;
     return os;
 }
 
@@ -89,7 +94,7 @@ std::basic_istream<CharT, Traits>&
 operator >> (std::basic_istream<CharT, Traits>& is,
              const pseudo_random_distribution& d)
 {
-    is >> d.parameters.parameter_c >> d.internal_n;
+    is >> d.parameters.parameter_c >> d.parameters.parameter_d >> d.internal_n;
     return is;
 }
 }
