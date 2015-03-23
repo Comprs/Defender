be914917861047b63168b86b202173ef95b82b2f
Peter Smith <comprs@outlook.com>
2015-03-23 19:29:26 +0000

    Made the update member function public as it should have been

--- fighter.h
+++ fighter.h
@@ -10,12 +10,12 @@ class Fighter : public Alien
 public:
     Fighter(Room& room, std::shared_ptr<Texture> texture);
     void interact(Entity& entity) override;
+    void update(const double time) override;
 
 protected:
     pseudo_random_distribution diveDistribution;
     pseudo_random_distribution swapDistrubution;
 
-    void update(const double time) override;
     void onKill() override;
 };
 }
